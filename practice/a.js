f150693183898017d2924219d25c9ad9ff7f80c6
yc2121 <yc2121@live.com>
2018-07-02 16:52:51 -0500

    more

---
+++ practice/a.js
@@ -0,0 +1,15 @@
+function fizzBuzz( n ) {
+	for (i=1;i<n+1;i++) {
+		if (n%3 == 0) {
+			if (n%5 == 0) {
+				console.log('FizzBuzz');
+			}
+			else
+				console.log('Fizz');
+		} else if (n%5 == 0) {
+			console.log('Buzz');
+		} esle
+			console.log(n);
+	}
+}
+
