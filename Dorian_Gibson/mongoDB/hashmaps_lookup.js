d210ae3c6ece92726d2504754470a65b243d472a
dgibs750 <doriangibson9663@gmail.com>
2018-07-11 12:14:28 -0500

    Added HashMaps in MongoDB, and BST-depth in Express

---
+++ Dorian_Gibson/mongoDB/hashmaps_lookup.js
@@ -0,0 +1,68 @@
+var hashMap = [];
+hashMap.length = 30;
+
+String.prototype.hashCode = function(){
+    var hash = 0;
+    if(this.length == 0){
+        return hash;
+    }
+    for(var i=0; i<this.length; i++){
+        var char = this.charCodeAt(i);
+        hash = ((hash<<5)-hash) + char;
+        hash &= hash;
+    }
+    return hash;
+}
+//var hashedKey = 'role'.hashCode();
+
+function mod(input, div){
+    return(input % div+div) % div;
+}
+//var idx = mod(hashedKey, hashMap.length);
+
+function hashInsert (key, value){
+    var hashedKey = key.hashCode();
+    var idx = mod(hashedKey, hashMap.length);
+    console.log(idx);
+    if(!hashMap[idx]){
+        hashMap[idx] = [];
+    }
+    for(var i=0; i<hashMap[idx].length; i++){
+        if(hashMap[idx][i][0] == key){
+            hashMap[idx][i][1] = value;
+            console.log('Updated hashMap ', hashMap);
+            return;
+        }
+    }
+    hashMap[idx].push([key, value]);
+    console.log('Added to hashMap ', hashMap);
+}
+
+function hashFind (map, key){
+    var hashedKey = key.hashCode();
+    var idx = mod(hashedKey, map.length);
+    if(map[idx]){
+        for(var i=0; i<map[idx].length; i++){
+            if(map[idx][i][0] == key){
+                console.log(map[idx][i][1]);
+                return;
+            }
+        }
+    }
+    else{
+        return null;
+    }
+}
+
+hashInsert('car','GTR');
+hashInsert('car','AE86');
+hashInsert('car','RX7');
+hashInsert('laptop','Macbook Pro');
+hashInsert('laptop','Razer Blade 15');
+hashInsert('desktop','iMac');
+hashInsert('desktop','Custom Build');
+
+hashFind(hashMap,'car');
+hashFind(hashMap,'laptop');
+hashFind(hashMap,'truck');
+
