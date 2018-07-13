2aac3867d3b229d488b35081501fb9c8b8ad6dc4
DaseinUXD <markmatthewsphd@gmail.com>
2018-07-13 14:59:13 -0500

    Angular

--- markmatthewsphd/express/hello_express/server.js
+++ markmatthewsphd/express/hello_express/server.js
@@ -6,6 +6,11 @@ var app = express();
 
 console.log('Lets find out what app is', app);
 
+app.use(express.static(__dirname + '/static'));
+
+app.set('views', __dirname + '/views');
+
+app.set('view engine', 'ejs');
 
 app.get('/', function (request, response) {
     console.log(' this is the request object', request);
@@ -14,8 +19,18 @@ app.get('/', function (request, response) {
     response.send("<h1>hello express</h1>");
 
 
-} );
+});
 
-app.listen(666, function (){
+app.get('/users', function (request, response) {
+    var users_array = [
+        {name: 'michael', email: 'michael@cd.com'},
+        {name: 'jay', email: 'jay@cd.com'},
+        {name: 'brendan', email: 'brendan@cd.com'},
+        {name: 'andrew', email: 'andrew@cd.com'}
+    ];
+    response.render('users', {users: users_array});
+});
+
+app.listen(666, function () {
     console.log('listing on port 666');
 });
