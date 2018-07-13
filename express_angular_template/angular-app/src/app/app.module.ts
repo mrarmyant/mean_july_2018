310abaa58b409b0d8c1699065c6925f5035dcd04
josh reese <droptablestar@gmail.com>
2018-07-13 10:26:59 -0500

    Forgot HttpClientModule import in app.module.ts

--- express_angular_template/angular-app/src/app/app.module.ts
+++ express_angular_template/angular-app/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http'
+
 import { AppComponent } from './app.component';
 
 @NgModule({
